698eb1c97f39c240703d9bc1af19ffa96c5de8cb
Rad <145648587+not-dika@users.noreply.github.com>
2024-05-21 16:35:47 +0000

    Fix Bugs

--- HOMEWORK 8/homework8.py
+++ HOMEWORK 8/homework8.py
@@ -11,22 +11,41 @@ print("5. Tampilkan Semua Data Barang")
 print("6. Tampilkan Jumlah Data Tersimpan")
 print("7. Keluar\n")
 while True:
-    choise = int(input("Masukan Pilihan : "))
-    if choise == 1:
+    choise = input("Masukan Pilihan : ")
+    if choise == "1" or choise.lower() == "tambah data barang":
         nama = input("Masukan nama barang: ")
         stock = int(input("Masukan stock barang: "))
         db.tambah_data(database, nama, stock)
         print("")
-    elif choise == 2:
+    elif choise == "2" or choise.lower() == "edit data barang":
+        nama = input("Masukan nama barang: ")
+        db.edit_data(database, nama)
+        print("")
+    elif choise == "3" or choise.lower() == "hapus data barang":
         nama = input("Masukan nama barang: ")
         db.hapus_data(database, nama)
         print("")
-    elif choise == 3:
-        nama = input("Masukan nama barang atau 'Semua' untuk semua barang: ")
+    elif choise == "4" or choise.lower() == "cari data barang":
+        nama = input("Masukan nama barang: ")
         db.tampilkan_data(database, nama)
         print("")
-    elif choise == 4:
+    elif choise == "5" or choise.lower() == "tampilkan semua data barang":
+        db.tampilkan_data(database, "semua")
+        print("")
+    elif choise == "6" or choise.lower() == "tampilkan jumlah data tersimpan":
+        print(f"Jumlah Data Tersimpan: {len(database)} Barang")
+        print("")
+    elif choise == "7" or choise.lower() == "keluar":
         print("Bye Bye!")
         exit()
     else:
+        print("Program Manajemen Stock Barang")
+        print("Pilihan:")
+        print("1. Tambah Data Barang")
+        print("2. Edit Data Barang")
+        print("3. Hapus Data Barang")
+        print("4. Cari Data Barang")
+        print("5. Tampilkan Semua Data Barang")
+        print("6. Tampilkan Jumlah Data Tersimpan")
+        print("7. Keluar\n")
         print("Pilihan Tidak Valid!")
--- HOMEWORK 8/modul8.py
+++ HOMEWORK 8/modul8.py
@@ -2,6 +2,34 @@ def tambah_data(db, item, stock):
     db.append({"Nama" : item, "Stock" : stock})
     print("Berhasil menambahkan data")
 
+def tampilkan_data(db, item):
+    if len(db) == 0:
+        print("Data barang kosong.")
+    elif item.lower() == "semua":
+        print("List data barang yang tersimpan: ")
+        for i in range(len(db)):
+            print(f"- {db[i]['Nama']}, Stock: {db[i]['Stock']}")
+    else:
+        found = 0
+        for i in range(len(db)):
+            if item in db[i]['Nama']:
+                if found == 0:
+                    print(f"Data barang dengan nama {item}:")
+                found += 1
+                print(f"- {db[i]['Nama']}, Stock: {db[i]['Stock']}")
+        if found == 0:
+                print(f"Data barang dengan nama {item} tidak ditemukan")
+
+def update_data(db, item):
+    found = 0
+    for i in range(len(db)):
+        if db[i]['Nama'] == item:
+            found += 1
+            db[i]['Stock'] = int(input("Masukan Stock Baru: "))
+            print("Berhasil mengedit data")
+    if found == 0:
+        print(f"Tidak ada barang bernama {item}")
+
 def hapus_data(db, item):
     found = 0
     for i in range(len(db)):
@@ -11,19 +39,3 @@ def hapus_data(db, item):
             print("Berhasil menghapus data")
     if found == 0:
         print(f"Tidak ada barang bernama {item}")
-
-def tampilkan_data(db, item):
-    if item.lower() == "semua":
-        print("List data barang yang tersimpan: ")
-        for i in range(len(db)):
-            print(db[i])
-    else:
-        found = 0
-        for i in range(len(db)):
-            if item in db[i]['Nama']:
-                if found == 0:
-                    print(f"Data barang dengan nama {item}:")
-                found += 1
-                print(db[i])
-        if found == 0:
-                print(f"Data barang dengan nama {item} tidak ditemukan")
